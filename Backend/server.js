5224e35ac49170874f66baf833157f5dc0a29582
ayoubjebb2001 <jebbouri1968@gmail.com>
2025-12-08 15:18:22 +0100

    TRAN-16 CRUD camions

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,7 @@ const connectDB = require('./config/database');
 
 const errorHandler = require('./middlewares/error');
 const auth = require('./middlewares/auth');
+const admin = require('./middlewares/admin');
 
 const app = express();
 
@@ -13,12 +14,14 @@ app.use(cors());
 app.use(express.json());
 
 const authRoutes = require('./routes/authRoutes');
+const camionRoutes = require('./routes/camionRoutes');
 
 app.get('/health', (req, res) => {
     res.json({ status: 'ok' });
 });
 
 app.use('/api/auth', authRoutes);
+app.use('/api/admin/camions', auth, admin, camionRoutes);
 
 app.route('/api/protected').get(auth, (req, res) => {
     res.send('Protected route');
